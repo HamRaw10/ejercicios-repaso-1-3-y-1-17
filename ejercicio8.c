c2575f0fffe61951f22ba694d2895277b319c28e
Maximo Gonzalez <136939649+HamRaw10@users.noreply.github.com>
2024-04-08 17:43:45 -0300

    Create ejercicio8.c

---
+++ ejercicio8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]) {
+ 	float invertir;
+ 	scanf("%f", &invertir);
+	 printf("El sueldo que obtendra al final es: %.2f", invertir*1.2); 
+
+return 0;
+}
